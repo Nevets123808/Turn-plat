902a91d7e1a6939a179189f5eaea2cbdd120a2df
Steven Bourn <stevenbourn2@hotmail.com>
2021-11-02 13:58:19 +0000

    Started main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import pygame
+from pygame.locals import *
+
+
+
+SCREENWIDTH = 640
+SCREENHEIGHT = 480
+
+
+active = True
+
 def main():
+    while active:
+        for event in pygame.event.get():
+            if event.type == quit():
+                active = False
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode([SCREENWIDTH,SCREENHEIGHT])
+    clock = pygame.time.Clock()
+    main()
+    pygame.quit()

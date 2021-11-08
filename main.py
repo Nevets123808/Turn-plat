63c204c39fabde18249a9f2000d2fb31cbdffa64
Steven Bourn <stevenbourn2@hotmail.com>
2021-11-08 13:46:27 +0000

    added basic falling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import math as maths
 
 
 class Player:
@@ -11,24 +12,27 @@ class Player:
         self.dx = 0
         self.dy = 0
         self.rect = pygame.Rect((16*self.x, 16*self.y),(self.width, self.height))
+        self.landed = False
     
     def draw(self, screen):
         self.rect = pygame.Rect((16*self.x, 16*self.y),(self.width, self.height))
         pygame.draw.rect(screen, (255,0,0), self.rect, width=0)
-        text = f"{self.dx}, {self.dy}"
+        text = f"{self.x}, {self.y}\n{self.dx}, {self.dy}"
         text_surface = FONT.render(text, True, (0,0,0))
         screen.blit(text_surface, (1,1))
 
     def input(self, event):
-        if event.key == K_w:
-            self.dy -= 1
-        elif event.key == K_x:
-            self.dy += 1
-        elif event.key == K_a:
-            self.dx -= 1
-        elif event.key == K_d:
-            self.dx += 1
 
+        if self.landed:
+            if event.key == K_w:
+                self.dy -= max(2,maths.floor(maths.sqrt(abs(self.dx))))
+                self.landed = False
+            elif event.key == K_a:
+                self.dx -= 1
+            elif event.key == K_d:
+                self.dx += 1
+        else:
+            self.dy += 1
         self.x += self.dx 
         self.y += self.dy
 
@@ -39,13 +43,20 @@ def main():
     active = True
     turn = True
     player = Player(2,2)
+    
     while active:
+        if player.y >= 20:
+            player.landed = True
+            player.y = 20
+            player.dy = 0
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 active = False
             if turn and event.type == KEYDOWN:
                 player.input(event)
         screen.fill((255,255,255))
+        pygame.draw.rect(screen, (125,125,125),(0,22*16,640,32),width=0)
         player.draw(screen)
         pygame.display.flip()
 
